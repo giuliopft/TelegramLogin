5631e9833e9d95052673d03af528fae4e3237336
giuliopft <giulio.ticli04@gmail.com>
2020-09-26 21:31:28 +0200

    Javadocs for BotCommandFactory.java

--- src/main/java/io/github/giuliopft/telegramlogin/bot/commands/BotCommandFactory.java
+++ src/main/java/io/github/giuliopft/telegramlogin/bot/commands/BotCommandFactory.java
@@ -4,13 +4,31 @@ import io.github.giuliopft.telegramlogin.TelegramLogin;
 
 import java.util.Arrays;
 
+/**
+ * This class generates {@link BotCommand} instances.
+ */
 public class BotCommandFactory {
+    /**
+     * Main class instance.
+     */
     private final TelegramLogin telegramLogin;
 
+    /**
+     * Creates a new BotCommandFactory.
+     *
+     * @param telegramLogin Main class instance.
+     */
     public BotCommandFactory(TelegramLogin telegramLogin) {
         this.telegramLogin = telegramLogin;
     }
 
+    /**
+     * Creates and returns an instance of a subclass of {@link BotCommand}.
+     *
+     * @param chatId The chat where the message was sent.
+     * @param text   The message, split where space characters occur.
+     * @return A {@link BotCommand} instance, or <code>null</code> if the message didn't contain any valid commands.
+     */
     public BotCommand getCommand(long chatId, String[] text) {
         switch (text[0]) {
             case ".start":
